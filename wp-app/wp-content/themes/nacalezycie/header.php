7b25e753cc5b07b5134b804f2773c53216ce35a3
greg cieslak <g.cieslak@asper.net.pl>
2020-04-07 09:41:38 +0200

    fix font in header

--- wp-app/wp-content/themes/nacalezycie/header.php
+++ wp-app/wp-content/themes/nacalezycie/header.php
@@ -12,32 +12,31 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
-
-	<?php wp_head(); ?>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'nacalezycie' ); ?></a>
-
-	<header id="masthead" class="site-header">
-        <nav class="navbar navbar-expand-lg navbar-light">
-            <a class="navbar-brand logoMenu__mobile" href="#">
-                <img class="logoMenu" src="<?php echo the_field('logo', 'option')?>" alt="logo">
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav menuLinks">
-                    <?php
+    <?php wp_body_open(); ?>
+    <div id="page" class="site">
+        <a class="skip-link screen-reader-text"
+            href="<?php echo get_home_url() ?>"><?php esc_html_e( 'Skip to content', 'nacalezycie' ); ?></a>
+        <header id="masthead" class="site-header">
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <a class="navbar-brand logoMenu__mobile" href="#">
+                    <img class="logoMenu" src="<?php echo the_field('logo', 'option')?>" alt="logo">
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav menuLinks ">
+                        <?php
                     wp_nav_menu( array(
                         'menu'        => 'MainMenu1',
                         'container' => '',
@@ -45,23 +44,23 @@
                         'items_wrap' => '%3$s'
                     ) );
                     ?>
-                    <li class="nav-item logoMenu__desktop">
-                        <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">
-                            <img class="logoMenu" src="<?php echo the_field('logo', 'option')?>" alt="logo">
-                        </a>
-                    </li>
-                    <?php wp_nav_menu( array(
+                        <li class="nav-item logoMenu__desktop">
+                            <a class="nav-link" href="<?php echo get_home_url() ?>" tabindex="-1" aria-disabled="true">
+                                <img class="logoMenu" src="<?php echo the_field('logo', 'option')?>" alt="logo">
+                            </a>
+                        </li>
+                        <?php wp_nav_menu( array(
                     'menu'        => 'MainMenu2',
                     'container' => '',
                     'menu_class'      => 'MenuLinks',
                     'items_wrap' => '%3$s'
                     ) );
                     ?>
-                </ul>
-            </div>
-        </nav>
+                    </ul>
+                </div>
+            </nav>
 
 
-	</header><!-- #masthead -->
+        </header><!-- #masthead -->
 
-	<div id="content" class="site-content">
+        <div id="content" class="site-content">
