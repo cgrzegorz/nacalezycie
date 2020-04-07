0c3f910d5123e18e0cc1838fa87216e10174aea4
greg cieslak <g.cieslak@asper.net.pl>
2020-04-07 22:58:21 +0200

    add telling stories section

--- wp-app/wp-content/themes/nacalezycie/template-parts/sections/telling_story/telling_story.php
+++ wp-app/wp-content/themes/nacalezycie/template-parts/sections/telling_story/telling_story.php
@@ -1,1 +1,34 @@
-<p>Kutas historie z dupy</p>
+<div class="tellingStories">
+    <div class="row no-gutters">
+        <div class="col-7">
+            <div class="tellingStories__first">
+                <p class="tellingStories__first__title">Geschichten<br> erzählen...</p>
+                <div class="tellingStories__first__desc">
+                    <p class="tellingStories__first__desc__text">
+                        Menschen berühren. Geschichten erzählen. Momente für die Ewigkeit einfrieren – Das ist es, worum
+                        es geht. Es geht um EUCH und eure Liebe. Wir möchten euch an eurem Tag wie gute Freunde ein
+                        Stück auf eurem Weg begleiten und für euch alle großen und kleinen Gänsehautmomente authentisch
+                        und ungestellt einfangen. Natürlich, unverfälscht und echt. Wir kreieren Erinnerungen, die euch
+                        für immer bleiben, ganz egal wo auf der Welt. Wenn ihr auf der Suche nach authentischen und
+                        emotionalen Hochzeitsbildern und -filmen seid, dann seid ihr bei uns genau richtig!
+                    </p>
+                    <img class="tellingStories__first__desc__img"
+                        src="https://www.vickybaumann.de/wp-content/uploads/2018/11/Vicky-Babak-345x540-276x432.jpg" />
+                        <div class="tellingStories__first__small_section">
+                            <p>AUTHENTISCHE<br>HOCHZEITSBILDER</p>
+                            <img src="https://www.vickybaumann.de/wp-content/uploads/2019/04/Flower-Element-schwarz-2-61x140.png" alt="flower"/>
+                            <p>EMOTIONALE<br>HOCHZEITSFILME</p>
+                        </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-5">
+            <div class="tellingStories__second">
+                <img src="https://www.vickybaumann.de/wp-content/uploads/2019/01/Vicky-Babak-2-1044x696.jpg" alt="couple">
+                <div class="tellingStories__second__text">
+                    <p>Es ist euer Tag.<br>Wir sind für euch da!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
