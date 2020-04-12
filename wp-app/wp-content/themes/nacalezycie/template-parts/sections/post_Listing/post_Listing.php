20af30a61afa20cde713d002c8db2fc184947311
greg cieslak <g.cieslak@asper.net.pl>
2020-04-12 15:51:04 +0200

    add listing posts

--- wp-app/wp-content/themes/nacalezycie/template-parts/sections/post_Listing/post_Listing.php
+++ wp-app/wp-content/themes/nacalezycie/template-parts/sections/post_Listing/post_Listing.php
@@ -1,35 +1,15 @@
-<div class="postListing">
-    <div class="row">
-        <a href="#" class="col-12 col-md-4 postListing__item">
-            <img src="https://fakeimg.pl/350x200"/>
-            <h4>Free Spirit Wedding auf Bali</h4>
-            <p> Diese spirituelle und emotionale Hochzeit auf Bali war in so vielen Punkten überwältigend! Kamilla und Andy, zwei ganz besondere Menschen mit einer tiefen Verbundenheit zur Natur und ihrer Umwelt - und zu Bali.</p>
-        </a>
-        <div class="col-12 col-md-4 postListing__item">
-            
-            <img src="https://fakeimg.pl/350x200"/>
-            <h4>Free Spirit Wedding auf Bali</h4>
-            <p> Diese spirituelle und emotionale Hochzeit auf Bali war in so vielen Punkten überwältigend! Kamilla und Andy, zwei ganz besondere Menschen mit einer tiefen Verbundenheit zur Natur und ihrer Umwelt - und zu Bali.</p>
-            
-        </div>
-        <div class="col-12 col-md-4 postListing__item">
-            <img src="https://fakeimg.pl/350x200" />
-            <h4>Free Spirit Wedding auf Bali</h4>
-            <p> Diese spirituelle und emotionale Hochzeit auf Bali war in so vielen Punkten überwältigend! Kamilla und Andy, zwei ganz besondere Menschen mit einer tiefen Verbundenheit zur Natur und ihrer Umwelt - und zu Bali.</p>
-        </div>
-       
-
-
-
-
-
+<div class="postListing container-fluid">
+    <div class="row justify-content-md-center">
+        <?php $posts = get_sub_field('Listing'); ?>
+        <?php foreach ( $posts as $post ){ ?>
+            <a href="<?php echo $post['post_Listing-post-item']->guid; ?>" class="col-12 col-md-3 postListing__item">
+                <div class="kupa"><img  src="<?php echo get_the_post_thumbnail_url($post['post_Listing-post-item']->ID) ?>" /></div>
+                <h4 class="postListing__item__title"><?php echo $post['post_Listing-post-item']->post_title; ?></h4>
+                <p class="postListing__item__desc"><?php echo $post['post_Listing-post-item']->post_excerpt; ?></p>
+            </a>
+        <?php
+} ?>
     </div>
-
-
 </div>
 
 
-
-<!-- get_sub_field('Listing')
-
-get_the_post_thumbnail('122') -->
