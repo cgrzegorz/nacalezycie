0f84514ed235ceea3277f955c45bd8420c1dc1e8
greg cieslak <g.cieslak@asper.net.pl>
2020-04-12 18:46:33 +0200

    doone mobile post listing

--- wp-app/wp-content/themes/nacalezycie/template-parts/sections/post_Listing/post_Listing.php
+++ wp-app/wp-content/themes/nacalezycie/template-parts/sections/post_Listing/post_Listing.php
@@ -2,8 +2,8 @@
     <div class="row justify-content-md-center">
         <?php $posts = get_sub_field('Listing'); ?>
         <?php foreach ( $posts as $post ){ ?>
-            <a href="<?php echo $post['post_Listing-post-item']->guid; ?>" class="col-12 col-md-3 postListing__item">
-                <div class="kupa"><img  src="<?php echo get_the_post_thumbnail_url($post['post_Listing-post-item']->ID) ?>" /></div>
+            <a href="<?php echo $post['post_Listing-post-item']->guid; ?>" class="col-12 col-md-4 col-lg-3 postListing__item">
+                <div class="postListing__imgHover"><img  src="<?php echo get_the_post_thumbnail_url($post['post_Listing-post-item']->ID) ?>" /></div>
                 <h4 class="postListing__item__title"><?php echo $post['post_Listing-post-item']->post_title; ?></h4>
                 <p class="postListing__item__desc"><?php echo $post['post_Listing-post-item']->post_excerpt; ?></p>
             </a>
