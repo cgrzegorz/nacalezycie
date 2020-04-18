ddb76bb6876764121aa428e1a0a1e8a85ac96469
grzegorz <cgrzegorz@gmail.com>
2020-04-18 14:04:19 -0700

    add new wedding

--- wp-app/wp-content/themes/nacalezycie/template-parts/sections/post-content/post-content.php
+++ wp-app/wp-content/themes/nacalezycie/template-parts/sections/post-content/post-content.php
@@ -3,21 +3,20 @@
     <div class="row no-gutters">
         <div class="postContent__text col-12 col-md-12">
             <?php echo get_sub_field('post_text') ?></div>
-
     </div>
 </div>
 <div class="row no-gutters">
-    
+
 </div>
 <div class="container">
-<div class="postContent__music ">
+    <div class="postContent__music ">
         <?php echo get_sub_field('post-image-music') ?>
     </div>
-    </div>
+</div>
 <div class="row no-gutters justify-content-center">
 
-    <div class="postContent__img col-12 col-md-7">
-        
+    <div class="postContent__img col-12 col-md-6">
+
         <?php if( $images ): ?>
         <?php foreach( $images as $key => $image_id ): ?>
         <img class="img-fluid postContent__img__item" src="<?php echo esc_url($image_id['sizes']['post-blog']);?>"
